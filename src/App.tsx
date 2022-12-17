598d32637ca7cae8ef67ed74ab98c07d07004dbf
Christina Labay <Christina.m.labay@gmail.com>
2022-12-17 16:39:24 -0600

    set up base

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { LandingContainer} from "./layouts/landing"
+
+function App() {
+  return (
+    <div className="App">
+      <LandingContainer />
+    </div>
+  );
+}
+
+export default App;
