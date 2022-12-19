c1131d0f8e6c4578b72b756007773e0e6bea599b
Christina Labay <Christina.m.labay@gmail.com>
2022-12-18 18:04:47 -0600

    add basic routes

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import { RoutesContainer } from "./routes-container";
+
+function App() {
+  return (
+    <div className="App">
+      <RoutesContainer />
+    </div>
+  );
+}
+
+export default App;
